6b5709939fbd3697a29c09c0061d0e351cc99099
wblaiklock <wblaiklock@gmail.com>
2025-01-27 10:57:44 -0500

    Initial

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const deck = new Deck();
+deck.get().then(()=>{
+
+    document.getElementById('draw').addEventListener('click', drawEvent);
+    document.getElementById('draw2').addEventListener('click', (e)=>drawEvent(e,2));
+    document.getElementById('draw5').addEventListener('click', (e)=>drawEvent(e,5));
+    document.getElementById('drawDiscard').addEventListener('click', drawDiscardEvent);
+    
+
+    
+    deck.shuffle().then(res=>{console.log(res)})
+    deck.draw()
+        .then(res=>{
+            console.log(res)          
+            deck.discard(res.cards[0].code);
+        });
+    deck.draw()
+        .then(res=>{
+            console.log(res.cards[0])            
+            deck.discard(res.cards[0].code);
+        });
+
+
+   
+   function drawEvent(e, num=1){
+       console.log("Drawing "+num)
+        deck.draw(num).then(res=>{
+            console.log(res.cards);
+            for(c in res.cards){
+                let card = document.createElement("IMG");
+                card.src = res.cards[c].images.png;
+                document.getElementsByTagName('body')[0].appendChild(card)
+            }
+        });
+   }
+   
+   function drawDiscardEvent(){
+        deck.drawFromPile().then(res=>{
+            if(res){
+                for(c in res.cards){
+                    let card = document.createElement("IMG");
+                    card.src = res.cards[c].images.png;
+                    document.getElementsByTagName('body')[0].appendChild(card)
+                }
+            }
+        });
+   }
+    
+});
